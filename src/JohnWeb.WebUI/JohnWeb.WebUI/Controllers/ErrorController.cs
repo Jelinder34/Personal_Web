816ad8a17e2f4c23c60849b1aa297b40393f246a
John Linder <john.linder@waystar.com>
2019-10-09 09:52:51 -0400

    Pushing project up to github

---
+++ src/JohnWeb.WebUI/JohnWeb.WebUI/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JohnWeb.WebUI.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult NotFound(int id)
+        {
+            Response.StatusCode = id;
+            return View();
+        }
+    }
+}
---
+++ src/JohnWeb.WebUI/JohnWeb.WebUI/Scripts/gAnalytics.js
@@ -0,0 +1,4 @@
+﻿$('#ResumeLink').on('click', function () {
+    gtag('config', 'UA-149505992-1');
+    gtag('event', 'download', { 'send_to': 'UA-149505992-1' });
+});
